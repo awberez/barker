2124d24e31174d3434b23dd41c4020d3058ee52d
awberez <alex@berez.com>
2018-03-03 10:31:32 -0600

    form completion required

--- client/src/Components/Discover/Discover.js
+++ client/src/Components/Discover/Discover.js
@@ -3,13 +3,26 @@ import API from "../../utils/API";
 import './Discover.css';
 
 class Discover extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+        }
+    }
 
+	componentDidMount() {
+	  	let userObj = {userId: this.props.userId}
+	  	if (!userObj.userId) this.props.history.push('/');
+	  	console.log(this.props.match);
+
+	  }
 
     render () {
         return (
             <div className='flex-container flex-end flex-space'>
-                
+                <p>discover</p>
             </div>
         )
     }
 }
+
+export default Discover;
