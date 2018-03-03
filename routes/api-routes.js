602714dd2cc0431e33f2b69c15f616472281816e
awberez <alex@berez.com>
2018-03-03 11:17:35 -0600

    discover route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,7 +30,7 @@ module.exports = (app, passport)=>{
 		            db.User.create({user_login: user_login, user_passwd: hash})
 		            .then(newUser => {
 		                res.json(newUser);
-		            })   
+		            })  
 		        })
 		    }
 	    });
@@ -86,6 +86,24 @@ module.exports = (app, passport)=>{
 			});
 		});
 	});
+
+	app.get('/api/matches/:id', (req, res)=>{
+       db.User.findOne({
+			where: {
+			   id: req.params.id
+			}
+			}).then(user => {db.User.findAll({
+			   where: {
+			       city: user.city,
+			       id: {
+			           [Op.ne]: req.params.id
+			       }
+			   },
+			   attributes: ['id']
+			}).then(ids => {res.json(ids);
+       		});
+       });
+   });
 };
 
 
