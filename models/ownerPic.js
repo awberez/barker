b973584737e2af27bca711b2b17bbe30d7e332a1
rtgiles <rtgiles@msn.com>
2018-03-03 10:57:13 -0600

    ownerPic model added

---
+++ models/ownerPic.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes)=>{}
+const picOwner = sequelize.define('user', {
+    id: {type: Sequelized.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true
+    }, 
+    owner_id: {type: Sequelized.INT,
+        allowNull: false,
+        REFERENCES: {model: 'user',
+                       key: 'id'}
+    }, 
+    pic_name: {type: Sequelized.STRING(50)
+    },
+    pic_ref: {type: Sequelized.STRING (50)
+    },/*  -- for future use to store CDN reference */
+    pic: {type: Sequelized.BLOB
+    }, /* -- pic storage in database */
+})
