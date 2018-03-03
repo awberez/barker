4414f666fc3e30a4c36ee245d65c4b1c47a65e49
rtgiles <rtgiles@msn.com>
2018-03-03 12:47:02 -0600

    google api and ownerPic model added

---
+++ models/ownerPic.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes)=>{
+const picOwner = sequelize.define('picOwner', {
+    id: {type: Sequelized.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true
+    }, 
+    owner_id: {type: Sequelized.INT,
+        allowNull: false,
+        REFERENCES: {model: 'user',
+                       key: 'id'}
+    }, 
+    pic_name: {type: Sequelized.STRING(50)
+    },
+    pic_ref: {type: Sequelized.STRING (50)
+    },/*  -- for future use to store CDN reference */
+    pic: {type: Sequelized.BLOB
+    }, /* -- pic storage in database */
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+});
+return picOwner
+}
