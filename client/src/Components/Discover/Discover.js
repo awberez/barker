be3871b221fe03ac2773cb37b03a7fbdaa89caa2
awberez <alex@berez.com>
2018-03-03 11:05:38 -0600

    basic discover page

--- client/src/Components/Discover/Discover.js
+++ client/src/Components/Discover/Discover.js
@@ -6,20 +6,51 @@ class Discover extends Component {
     constructor (props) {
         super(props);
         this.state = {
+        	userMatches: [],
+        	dogMatches: []
         }
     }
 
 	componentDidMount() {
 	  	let userObj = {userId: this.props.userId}
 	  	if (!userObj.userId) this.props.history.push('/');
-	  	console.log(this.props.match);
-
-	  }
+	  	API.getUser(userObj)
+	      .then(res => {
+	      	if (!res.data.dog) {
+	      	    this.props.finishForm();
+	      	    this.props.history.push('/');
+	        }
+	        else {
+	        	API.findMatches(userObj)
+			      .then(res => {
+			      	this.setState({ matches: res.data.matches }, ()=>{
+			      		this.state.matches.forEach(id => {
+			      			let userObj = {userId: id};
+			      			API.getUser(userObj)
+						      .then(res => {
+						      	if (res.data.dog) {
+						      	    let userMatches = this.state.userMatches, dogMatches = this.state.dogMatches;
+						      	    userMatches.push(res.data.user);
+						      	    dogMatches.push(res.data.dog);
+						      	    this.setState({userMatches, dogMatches});
+						        }
+						      })
+						      .catch(err => console.log(err));
+			      		});
+			      	});
+			      })
+			      .catch(err => console.log(err));
+	        }
+	      })
+	      .catch(err => console.log(err));
+	}
 
     render () {
         return (
             <div className='flex-container flex-end flex-space'>
-                <p>discover</p>
+                {this.state.userMatches.map(user => (
+                <p>{user.fname}</p>
+              ))}
             </div>
         )
     }
