5a0a85c8e220f2b43c9c80e1357ed88de2f631ad
awberez <alex@berez.com>
2018-03-04 17:56:50 -0600

    location matching based on coordinates

--- client/src/Components/Discover/Discover.js
+++ client/src/Components/Discover/Discover.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import API from "../../utils/API";
 import DiscoverCard from "../DiscoverCard";
 import Header from "../Header";
+import geolib from "geolib";
 import './Discover.css';
+
+let userCoords = [], userObj;
 
 class Discover extends Component {
     constructor (props) {
@@ -12,7 +15,7 @@ class Discover extends Component {
     }
 
 	componentDidMount() {
-	  	let userObj = {userId: this.props.userId}
+	  	userObj = {userId: this.props.userId}
 	  	if (!userObj.userId) this.props.history.push('/');
 	  	API.getUser(userObj)
 	      .then(res => {
@@ -21,30 +24,44 @@ class Discover extends Component {
 	      	    this.props.history.push('/');
 	        }
 	        else {
-	        	API.findMatches(userObj)
-			      .then(res => {
-			      	this.setState({ matches: res.data, userMatches: [], dogMatches: [] }, ()=>{
-			      		console.log(res);
-			      		this.state.matches.forEach(id => {
-			      			console.log(id);
-			      			let userObj = {userId: id.id};
-			      			API.getUser(userObj)
-						      .then(res => {
-						      	if (res.data.dog) {
-						      	    let userMatches = this.state.userMatches, dogMatches = this.state.dogMatches;
-						      	    userMatches.push(res.data.user);
-						      	    dogMatches.push(res.data.dog);
-						      	    this.setState({userMatches, dogMatches}, ()=>{console.log(dogMatches);});
-						        }
-						      })
-						      .catch(err => console.log(err));
-			      		});
-			      	});
-			      })
-			      .catch(err => console.log(err));
+	        	userCoords = res.data.user.geoLocat.coordinates;
+	        	this.getMatches(16100);
 	        }
 	      })
 	      .catch(err => console.log(err));
+	}
+
+	getMatches = radius => {
+    	API.findMatches(userObj)
+	      .then(res => {
+	      	this.setState({ matches: res.data, userMatches: [], dogMatches: [] }, ()=>{
+	      		console.log(res);
+	      		this.state.matches.forEach(id => {
+	      			console.log(id);
+	      			let userObj = {userId: id.id};
+	      			API.getUser(userObj)
+				      .then(res => {
+				      	if (res.data.dog && this.withinDist(radius, res.data.user.geoLocat.coordinates)) {
+				      	    let userMatches = this.state.userMatches, dogMatches = this.state.dogMatches;
+				      	    userMatches.push(res.data.user);
+				      	    dogMatches.push(res.data.dog);
+				      	    this.setState({userMatches, dogMatches});
+				        }
+				      })
+				      .catch(err => console.log(err));
+	      		});
+	      	});
+	      })
+	      .catch(err => console.log(err));
+	}
+
+	withinDist = (radius, matchCoords) => {
+		console.log("getting distance");
+		let dist = geolib.getDistance(
+		    {latitude: userCoords[0], longitude: userCoords[1]},
+		    {latitude: matchCoords[0], longitude: matchCoords[1]}
+		);
+		return dist <= radius;
 	}
 
 	profileButton = () => {
@@ -56,6 +73,12 @@ class Discover extends Component {
             <div>
             	<button className="button" onClick={this.profileButton}>Edit My Profile!</button>
             	<Header><h3>Potential Matches</h3></Header>
+            	<h5>Choose a New Match Range</h5>
+            	<button onClick={()=>this.getMatches(8050)}>5 Miles</button>
+            	<button onClick={()=>this.getMatches(16100)}>10 Miles</button>
+            	<button onClick={()=>this.getMatches(24150)}>15 Miles</button>
+            	<button onClick={()=>this.getMatches(32200)}>20 Miles</button>
+            	<button onClick={()=>this.getMatches(40250)}>25 Miles</button>
             	<div className="wrapper">
 	                {this.state && this.state.matches && 
 	                	this.state.userMatches.map(user => (
