a909980eee6d7a5e5e4789b60b52076b7e05634d
awberez <alex@berez.com>
2018-03-09 15:24:32 -0600

    fix

--- models/index.js
+++ models/index.js
@@ -7,10 +7,10 @@ var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+var Op        = Sequelize.Op;
 
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
