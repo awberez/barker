e16bef0d614fae920847bfc8bccda2d227ac5387
awberez <alex@berez.com>
2018-03-09 14:30:54 -0600

    test

--- server.js
+++ server.js
@@ -9,11 +9,7 @@
 
 // =============================================================
 var express = require("express");
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
 const path = require('path');
 
 // Sets up the Express App
@@ -25,21 +21,11 @@ var PORT = process.env.PORT || 3001;
 // Requiring our models for syncing
 var db = require("./models");
 
-require('./config/passport')(passport);
-
 // Sets up all dependecies
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(session({secret: 'anystringoftext',
-                 saveUninitialized: true,
-                 resave: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
 
 // Static directory
 const staticFiles = express.static(path.join(__dirname, 'client', 'build'));
@@ -47,19 +33,19 @@ const staticFiles = express.static(path.join(__dirname, 'client', 'build'));
 app.use(staticFiles);
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app, passport);
+require("./routes/api-routes.js")(app);
 
 //app.use('/*', staticFiles);
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });*/
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({alter: true}).then(function() {
+db.sequelize.sync({force:false}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
